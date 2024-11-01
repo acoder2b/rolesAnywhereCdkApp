4dbb37349e3c5101b1d92437357f27e70f13e824
kamalpreet82 <69133416+kamalpreet82@users.noreply.github.com>
2024-11-01 10:20:41 -0400

    SNS TOpic

--- bin/app.ts
+++ bin/app.ts
@@ -5,8 +5,6 @@ import { AppStack } from '../lib/app-stack';
 
 const app = new cdk.App();
 new AppStack(app, 'AppStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+
   },
-});
+);
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import { Topic } from 'aws-cdk-lib/aws-sns'; // Correct import for SNS Topic
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create a new SNS Topic
+    const topic = new Topic(this, 'MySnsTopic');
+    new cdk.CfnOutput(this, 'SnsTopicArn', { value: topic.topicArn });
+    new cdk.CfnOutput(this, 'SnsTopicName', { value: topic.topicName });
+    
 
   }
 }
