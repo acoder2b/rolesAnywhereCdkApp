e1eaadb97245a9fcd5e9fc9e645cd67c7a8cd5d5
kamalpreet82 <69133416+kamalpreet82@users.noreply.github.com>
2024-11-01 10:04:18 -0400

    Updated-Workflow,app

--- bin/app.ts
+++ bin/app.ts
@@ -5,5 +5,8 @@ import { AppStack } from '../lib/app-stack';
 
 const app = new cdk.App();
 new AppStack(app, 'AppStack', {
-
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
