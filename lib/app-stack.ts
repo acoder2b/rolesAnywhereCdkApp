081a92a964fba51ddf82d6aa1da64915974475d8
kamalpreet82 <69133416+kamalpreet82@users.noreply.github.com>
2024-11-01 10:29:57 -0400

    APp Change:

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -4,13 +4,17 @@ import { Construct } from 'constructs';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      ...props,
+      env: {
+        account: '909180469036', // Account ID should be a string
+        region: 'us-east-1',     // Region should be a string
+      },
+    });
 
     // Create a new SNS Topic
     const topic = new Topic(this, 'MySnsTopic');
     new cdk.CfnOutput(this, 'SnsTopicArn', { value: topic.topicArn });
     new cdk.CfnOutput(this, 'SnsTopicName', { value: topic.topicName });
-    
-
   }
 }
